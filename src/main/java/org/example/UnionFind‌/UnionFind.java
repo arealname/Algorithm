7e10138cf7b06a51124b3ca64289fa1c2961f05e
cwj <2058626192@qq.com>
2026-04-27 07:08:42 +0800

    unionfind

---
+++ src/main/java/org/example/UnionFind‌/UnionFind.java
@@ -0,0 +1,39 @@
+package org.example.UnionFind‌;
+
+public class UnionFind {
+
+    private int[] parent;
+
+    private int[] rank;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+    }
+}
